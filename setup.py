177ecada2c0cd56cdf8bf039e525c3cffe224667
longhao <hal.long@outlook.com>
2025-05-04 10:19:14 +0800

    feat: migrate to setup.py for version management with commitizen
    Signed-off-by: longhao <hal.long@outlook.com>

--- setup.py
+++ setup.py
@@ -1,29 +1,63 @@
 #!/usr/bin/env python
 """
-A minimal setup.py file to make pip happy.
+Setup script for py-dem-bones.
 """
 
 import os
 import site
-import sys
 
 # Ensure that the local package is preferred over an installed version
 site.addsitedir(os.path.abspath(os.path.dirname(__file__)))
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# Version - will be updated by commitizen
+version = "0.7.0"
+
+# Define package requirements
+install_requires = [
+    "numpy>=1.20.0",
+]
+
+extras_require = {
+    "dev": [
+        "nox>=2023.4.22",
+    ],
+    "test": [
+        "pytest>=7.0.0",
+        "pytest-cov>=4.0.0",
+    ],
+    "docs": [
+        "sphinx>=7.2.0",
+        "sphinx-rtd-theme>=1.2.0",
+        "myst-parser>=2.0.0",
+        "sphinx-gallery>=0.15.0",
+        "pillow>=10.0.0",
+        "matplotlib>=3.8.0",
+        "myst-sphinx-gallery>=0.3.1",
+        "myst-nb>=1.1.0",
+    ],
+    "docs-linux": [
+        "cairosvg>=2.8.0",
+    ],
+    "lint": [
+        "black>=23.0.0",
+        "ruff>=0.0.270",
+        "isort>=5.12.0",
+        "autoflake>=2.2.0",
+    ],
+    "build": [
+        "build>=0.10.0",
+        "twine>=4.0.2",
+    ],
+}
 
 if __name__ == "__main__":
-    # Check if we're using scikit-build-core
-    using_scikit_build = any(
-        arg.startswith("--build-option=") for arg in sys.argv
-    ) or os.environ.get("SKBUILD_CMAKE_VERBOSE") is not None
-
-    # When using scikit-build-core, directly call setup()
-    # Otherwise, use setuptools' setup() function
     setup(
         name="py-dem-bones",
+        version=version,
         description="Python bindings for the Dem Bones library",
-        long_description=open("README.md").read(),
+        long_description=open("README.md", encoding="utf-8").read(),
         long_description_content_type="text/markdown",
         author="Long Hao",
         author_email="hal.long@outlook.com",
@@ -36,18 +70,26 @@ if __name__ == "__main__":
             "License :: OSI Approved :: BSD License",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
-            "Programming Language :: Python :: 3.13",
             "Programming Language :: C++",
             "Topic :: Scientific/Engineering",
             "Topic :: Scientific/Engineering :: Artificial Intelligence",
             "Topic :: Multimedia :: Graphics :: 3D Modeling",
             "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-        python_requires=">=3.7",
+        python_requires=">=3.8",
+        install_requires=install_requires,
+        extras_require=extras_require,
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        project_urls={
+            "Homepage": "https://github.com/loonghao/py-dem-bones",
+            "Documentation": "https://loonghao.github.io/py-dem-bones",
+            "Issues": "https://github.com/loonghao/py-dem-bones/issues",
+            "Changelog": "https://github.com/loonghao/py-dem-bones/blob/main/CHANGELOG.md",
+        },
     )
