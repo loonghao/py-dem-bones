3ceb9e08252c79ffcdef19056afc587b1b7deec2
longhao <hal.long@outlook.com>
2025-05-05 23:22:26 +0800

    fix: update commitizen and setuptools_scm configuration
    Signed-off-by: longhao <hal.long@outlook.com>

--- docs/conf.py
+++ docs/conf.py
@@ -54,10 +54,10 @@ copyright = '2024, Long Hao'
 author = 'Long Hao'
 
 # The full version, including alpha/beta/rc tags
-release = '0.11.1'
+release = '0.12.0'
 
 # Major version
-version = '0.11.1'
+version = '0.12.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
